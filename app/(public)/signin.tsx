b99e096e3a7fecefc878f76273e599d52683fd03
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2024-01-16 22:07:51 -0800

    sign in works

--- app/(public)/signin.tsx
+++ app/(public)/signin.tsx
@@ -1,7 +1,9 @@
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
+import TextInputComponent from "../../components/inputs/TextInputComponent";
+import { useSignIn } from "@clerk/clerk-expo";
 
 export type NewUser = {
   email: string;
@@ -10,29 +12,68 @@ export type NewUser = {
   lastName: string;
 };
 
+export type Credentials = {
+  email: string;
+  password: string;
+};
+
 export default function signIn() {
-  const [newUser, setNewUser] = useState<NewUser | null>({
+  const [credentials, setCredentials] = useState<Credentials | null>({
     email: "",
     password: "",
-    firstName: "",
-    lastName: "",
   });
+  const { isLoaded, signIn, setActive } = useSignIn();
+  const router = useRouter();
+  const handleInputChange = (name: keyof Credentials, value: string) => {
+    // handle input change and update according state
+    setCredentials(
+      (prevState) =>
+        ({
+          ...prevState,
+          [name]: value || "",
+        } as Credentials)
+    );
+  };
+  const handleSignIn = async () => {
+    if (!isLoaded) return;
+
+    try {
+      if (credentials) {
+        const completedSignIn = await signIn.create({
+          identifier: credentials.email,
+          password: credentials.password,
+        });
+        await setActive({ session: completedSignIn.createdSessionId });
+        router.replace("/home");
+      }
+    } catch (error) {
+      console.error(JSON.stringify(error, null, 2));
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Sign In</Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="First Name"
-          style={styles.input}
-          placeholderTextColor={"#000"}
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <TextInput
+      <View>
+        <Text>Sign In</Text>
+        <TextInputComponent
           placeholder="Email"
-          style={styles.input}
-          placeholderTextColor={"#000"}
+          handleChange={(text) => handleInputChange("email", text)}
+          keyboardType="email-address"
+          autoCap="none"
         />
+        <TextInputComponent
+          placeholder="Password"
+          handleChange={(text) => handleInputChange("password", text)}
+          keyboardType="default"
+          isSecure
+          autoCap="none"
+        />
+        <View>
+          <View>
+            <TouchableOpacity onPress={handleSignIn} style={styles.buttonStyle}>
+              <Text style={styles.buttonText}>Sign In</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
       <View style={styles.inputContainer}>
         <Text>
@@ -62,5 +103,16 @@ const styles = StyleSheet.create({
     borderColor: "#000",
     padding: 10,
     borderRadius: 10,
+  },
+  buttonStyle: {
+    backgroundColor: "#000",
+    padding: 15,
+    borderRadius: 10,
+    width: 300,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
   },
 });
